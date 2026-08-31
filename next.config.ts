0913cc1d25a7baa838a7441218d494f2c7e0e7ff
MetaWebDevCEO <MetaWebDevCEO@users.noreply.github.com>
2026-08-31 10:44:12 -0600

    limite

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,11 @@ const isVercel = process.env.VERCEL === "1";
 
 const nextConfig: NextConfig = {
   ...(isVercel ? {} : { distDir: ".next-build" }),
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "1000mb",
+    },
+  },
 };
 
 export default nextConfig;
